15ab797175c0ed941154613ef59d06a34225e4d7
gbcodeur <gbcodeur@gmail.com>
2026-02-26 02:21:17 +0000

    Update app.py

--- app.py
+++ app.py
@@ -169,4 +169,4 @@ if __name__ == '__main__':
         print("\nAppuyez sur Ctrl+C pour arrêter le serveur")
         print("="*60 + "\n")
         
-        app.run(debug=True, host='0.0.0.0', port=5000)
+        app.run(debug=False, host='0.0.0.0', port=10000)
